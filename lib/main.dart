2fc2887388c402d823ad7b64621d48565232767b
hannikhan911 <hannikhan2006@gmail.com>
2024-09-15 15:50:01 +0100

    auth_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled4/views/auth/signup_screen.dart';
 
 
 void main() {
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: SignupScreen(),
     );
   }
 }
