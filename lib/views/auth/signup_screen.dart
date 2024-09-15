4d4fe8bc786e56ec370d046768525669659b1ef5
HadiKhan510 <abdulhadikhans3040@gmail.com>
2024-09-15 19:54:54 +0500

    signip_screen_MODIFIED

--- lib/views/auth/signup_screen.dart
+++ lib/views/auth/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled4/const/colors.dart';
 
 
 
@@ -7,6 +8,9 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backGroundColor,
+      appBar: AppBar(),
+    );
   }
 }
