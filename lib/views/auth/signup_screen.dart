d0bfcc74b1a6b935df94abb742d3c5feb9426136
HadiKhan510 <abdulhadikhans3040@gmail.com>
2024-09-17 11:56:52 +0500

    signip_screen_pictureD

--- lib/views/auth/signup_screen.dart
+++ lib/views/auth/signup_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:untitled4/const/colors.dart';
+import 'package:untitled4/const/images.dart';
+import 'package:untitled4/reuseables/Text.widget.dart';
+
 
 
 
@@ -10,7 +13,41 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backGroundColor,
-      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: MediaQuery.sizeOf(context).height*0.15,
+                width: MediaQuery.sizeOf(context).width*0.25,
+decoration: BoxDecoration(
+  borderRadius: BorderRadius.all(Radius.circular(10)),
+),
+child:Image(image: AssetImage(loginImage)),
+              ),
+            ],
+          ),
+
+Row(
+  children: [
+    SizedBox(width: 20,),
+LargeText(headText:'Login', fontsize: 20,fontweight:FontWeight.w700,)
+  ],
+),
+          SizedBox(height: 5,),
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              SmallText(smallText: 'Select your country and Enter your phone number', fontsize: 14,fontweight: FontWeight.w400,textcolor: smallTextColor,)
+            ],
+          ),
+          
+
+        ],
+      ),
+
     );
   }
 }
